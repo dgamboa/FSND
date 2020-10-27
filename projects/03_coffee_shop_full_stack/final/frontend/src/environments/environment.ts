36c669be3bf6817e0ef91c9c4705d07bbda6fc81
Daniel Gamboa <danielgamboa10@gmail.com>
2020-10-27 15:26:49 -0500

    Update domain prefix to exclude auth0.com

--- projects/03_coffee_shop_full_stack/final/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/final/frontend/src/environments/environment.ts
@@ -6,7 +6,7 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: 'coffee-shop-dgamboa.us.auth0.com', // the auth0 domain prefix
+    url: 'coffee-shop-dgamboa.us', // the auth0 domain prefix
     audience: 'menu', // the audience set for the auth0 app
     clientId: 'D4kL1MEd0k0w3PL8BvqjqQWxZWLVGWOz', // the client id generated for the auth0 app
     callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
