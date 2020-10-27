f2c5d11f37c147a4eca453ddc246dfed46334a30
Daniel Gamboa <danielgamboa10@gmail.com>
2020-10-27 15:04:05 -0500

    Update environment variables for frontend

--- projects/03_coffee_shop_full_stack/final/frontend/src/environments/environment.ts
+++ projects/03_coffee_shop_full_stack/final/frontend/src/environments/environment.ts
@@ -6,9 +6,9 @@ export const environment = {
   production: false,
   apiServerUrl: 'http://127.0.0.1:5000', // the running FLASK api server url
   auth0: {
-    url: '', // the auth0 domain prefix
-    audience: '', // the audience set for the auth0 app
-    clientId: '', // the client id generated for the auth0 app
-    callbackURL: 'http://localhost:8100', // the base url of the running ionic application. 
+    url: 'coffee-shop-dgamboa.us.auth0.com', // the auth0 domain prefix
+    audience: 'menu', // the audience set for the auth0 app
+    clientId: 'D4kL1MEd0k0w3PL8BvqjqQWxZWLVGWOz', // the client id generated for the auth0 app
+    callbackURL: 'http://localhost:8100', // the base url of the running ionic application.
   }
 };
